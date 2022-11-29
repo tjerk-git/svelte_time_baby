60a3bae185a9c39e57a79d23ef39b87f1484014a
Tjerk Dijkstra <71773101+tjerk-git@users.noreply.github.com>
2022-11-29 10:26:28 +0100

    Dingen!

--- src/lib/ShareSheet.svelte
+++ src/lib/ShareSheet.svelte
@@ -1,15 +1,25 @@
 <script>
   export let challenges;
+
+  let color = ["blue", "red", "orange"];
 </script>
 
 <div class="challengeCard">
-  {#each challenges as challenge}
-    <h1>{challenge.title}</h1>
+  {#each challenges as challenge, index}
+    <h1 class={color[index]}>{challenge.title}</h1>
   {/each}
 </div>
 
 <style>
-  .challengeCard{
-    
+  .blue {
+    border-bottom: 3px solid rgb(117, 117, 255);
+  }
+
+  .red {
+    border-bottom: 3px solid rgb(246, 113, 113);
+  }
+
+  .orange {
+    border-bottom: 3px solid rgb(251, 189, 103);
   }
 </style>
--- src/lib/Wheel.svelte
+++ src/lib/Wheel.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import Typewriter from "svelte-typewriter";
   import { fade } from "svelte/transition";
   import ShareSheet from "./ShareSheet.svelte";
@@ -72,43 +72,106 @@
   {/each}
 </h1>
 
-{#if taskNumLocked == false}
-  <Typewriter mode="scramble" wordInterval: 1>
+<br />
+
+<div class="card">
+  {#if taskNumLocked == false}
+    <Typewriter mode="scramble" wordInterval: 1>
+      <h1>{tasks[tasksNum].title}</h1>
+    </Typewriter>
+
+    <button on:click={() => spendToken(1)}>Lock</button>
+  {:else}
     <h1>{tasks[tasksNum].title}</h1>
-  </Typewriter>
-  // component for button with hide if no tokens
-  <button on:click={() => spendToken(1)}>Lock</button>
-{:else}
-  <h1>{tasks[tasksNum].title}</h1>
-{/if}
+  {/if}
 
-{#if contextsNumLocked == false}
-  <Typewriter mode="scramble" wordInterval: 1>
+  {#if contextsNumLocked == false}
+    <Typewriter mode="scramble" wordInterval: 1>
+      <h1>{contexts[contextsNum].title}</h1>
+    </Typewriter>
+    <button on:click={() => spendToken(2)}>Lock</button>
+  {:else}
     <h1>{contexts[contextsNum].title}</h1>
-  </Typewriter>
-  <button on:click={() => spendToken(2)}>Lock</button>
-{:else}
-  <h1>{contexts[contextsNum].title}</h1>
-{/if}
+  {/if}
 
-{#if audiencesNumLocked == false}
-  <Typewriter mode="scramble" wordInterval: 1>
+  {#if audiencesNumLocked == false}
+    <Typewriter mode="scramble" wordInterval: 1>
+      <h1>{audiences[audiencesNum].title}</h1>
+    </Typewriter>
+    <button on:click={() => spendToken(3)}>Lock</button>
+  {:else}
     <h1>{audiences[audiencesNum].title}</h1>
-  </Typewriter>
-  <button on:click={() => spendToken(3)}>Lock</button>
-{:else}
-  <h1>{audiences[audiencesNum].title}</h1>
-{/if}
+  {/if}
+</div>
 
-<button on:click={spinTheWheel} class="is-primary">Spin the wheel</button>
-<button on:click={share} class="is-primary">Save results</button>
+<br /><br />
+<button on:click={spinTheWheel} class="is-primary button">Spin the wheel</button
+>
+<br />
+<button on:click={share} class="is-primary green">Save results</button>
 
 {#if showModal}
   <Modal on:close={() => (showModal = false)}>
     <h2 slot="header">
-      Share
-      <small><em>adjective</em> mod·al \ˈmō-dəl\</small>
+      <small><em>Sharing is caring</em></small>
     </h2>
     <ShareSheet {challenges} />
   </Modal>
 {/if}
+
+<style>
+  .card {
+    border: 2px solid salmon;
+    padding: 1rem;
+    border-radius: 15px;
+  }
+
+  .green {
+    background-color: rgb(82, 129, 209);
+    color: white;
+    font-size: 1.2rem;
+  }
+
+  .button {
+    background-color: #e73dc8;
+    border: 0 solid #e5e7eb;
+    box-sizing: border-box;
+    color: #000000;
+    display: flex;
+    font-family: ui-sans-serif, system-ui, -apple-system, system-ui, "Segoe UI",
+      Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
+      "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+      "Noto Color Emoji";
+    font-size: 1.6rem;
+    font-weight: 700;
+    justify-content: center;
+    line-height: 1.75rem;
+    padding: 0.75rem 1.65rem;
+    position: relative;
+    text-align: center;
+    text-decoration: none #000000 solid;
+    text-decoration-thickness: auto;
+    width: 100%;
+    max-width: 460px;
+    position: relative;
+    cursor: pointer;
+    transform: rotate(-2deg);
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+  }
+
+  .button:focus {
+    outline: 0;
+  }
+
+  .button:after {
+    content: "";
+    position: absolute;
+    border: 1px solid #000000;
+    bottom: 4px;
+    left: 4px;
+    width: calc(100% - 1px);
+    height: calc(100% - 1px);
+  }
+</style>
